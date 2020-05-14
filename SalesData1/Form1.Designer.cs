3adfda2398ff0538912b40310e92a0030689c6bb
mr-subroutine <darrylshack@outlook.com>
2020-05-14 15:19:09 -0700

    added functionality for salesdata file creation

--- SalesData1/Form1.Designer.cs
+++ SalesData1/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.txtBoxBasicInfo = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.Location = new System.Drawing.Point(227, 41);
+            this.textBoxInfo.Multiline = true;
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.Size = new System.Drawing.Size(303, 433);
+            this.textBoxInfo.TabIndex = 0;
+            // 
+            // txtBoxBasicInfo
+            // 
+            this.txtBoxBasicInfo.Location = new System.Drawing.Point(34, 102);
+            this.txtBoxBasicInfo.Name = "txtBoxBasicInfo";
+            this.txtBoxBasicInfo.Size = new System.Drawing.Size(158, 47);
+            this.txtBoxBasicInfo.TabIndex = 1;
+            this.txtBoxBasicInfo.Text = "Get Data From File";
+            this.txtBoxBasicInfo.UseVisualStyleBackColor = true;
+            this.txtBoxBasicInfo.Click += new System.EventHandler(this.txtBoxBasicInfo_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Create File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(579, 519);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBoxBasicInfo);
+            this.Controls.Add(this.textBoxInfo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sales Data1 v.1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxInfo;
+        private System.Windows.Forms.Button txtBoxBasicInfo;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- SalesData1/Form1.cs
+++ SalesData1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,105 @@ namespace SalesData1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        bool isButton1Clicked = false;
+        string[] workerNames = new string[4];
+        string[] daysOfWeek = new string[5];
+        int[,] salesDataNumbers = new int[3, 4];
+
+        private void createFile(bool isButton1clicked)
+        {
+            // path to write files
+            string StartUpPath = Application.StartupPath;
+            string fileLocation = StartUpPath + @"\salesdata.txt";
+
+            // creates a file (this is needed so the application can be self-reliant)
+            string[] fileData = {"100", "70", "80", "60", "70", "90",
+                "100", "100", "90", "50", "70", "80", "90", "100", "51", "70", "75", "70", "80", "90",
+                "Jim", "Kristina", "Darryl", "William", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
+            string fileCheck = fileLocation;
+            if (File.Exists(fileCheck) && isButton1clicked == true)
+            {
+                MessageBox.Show("The file already exists");
+                isButton1Clicked = true;
+            }
+
+            else if (!File.Exists(fileCheck) && isButton1clicked == true)
+            {
+                File.WriteAllLines(@fileLocation, fileData);
+                MessageBox.Show("Data File Created!");
+            }
+
+            else if (!File.Exists(fileCheck) && isButton1clicked == false)
+            {
+                MessageBox.Show("Please click Create File button first.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // bool to check if create file has been clicked
+            isButton1Clicked = true;
+            createFile(isButton1Clicked);
+        }
+
+        private void displaylabelTop()
+        {
+            textBoxInfo.Text += "-Sales Data-";
+        }
+
+        private void txtBoxBasicInfo_Click(object sender, EventArgs e)
+        {
+            if (isButton1Clicked == false)
+            {
+                string StartUpPath = Application.StartupPath;
+                string fileLocation = StartUpPath + @"\salesdata.txt";
+                File.Delete(fileLocation);
+            }
+
+            // bool to check if create file has been clicked, which it hasn't in this crease prompting user to create a file first.
+            //isButton1Clicked = false;
+            createFile(isButton1Clicked);
+            displaylabelTop();
+
+            if (isButton1Clicked == true)
+            {
+                // gathers data first storing into arrays in a method
+                storeAllData();
+            }
+        }
+
+        private void storeAllData()
+        {
+            // path to read file
+            string StartUpPath = Application.StartupPath;
+            string fileLocation = StartUpPath + @"\salesdata.txt";
+
+            // store all numeric data
+            using (StreamReader read = new StreamReader(fileLocation))
+            {
+                // Read and display lines from the file until the end of
+                // the file is reached.
+                for (int row = 0; row < 3 ; row++)
+                {
+                    for (int col = 0; col < 4; col++)
+                    {
+                        salesDataNumbers[row, col] = Convert.ToInt32(read.ReadLine());
+                    }
+                }
+
+                for (int row = 0; row < 4; row++)
+                {
+
+                }
+            }
+
+            // store employee names
+
+
+            // store days of week
         }
     }
 }
