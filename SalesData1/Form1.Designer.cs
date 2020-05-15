953349b1fca48403f6c7bd2482fbed12f2ca8917
mr-subroutine <darrylshack@outlook.com>
2020-05-15 12:08:58 -0700

    data from text file now displays in window, btn disable functionality added

--- SalesData1/Form1.Designer.cs
+++ SalesData1/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.textBoxInfo = new System.Windows.Forms.TextBox();
-            this.txtBoxBasicInfo = new System.Windows.Forms.Button();
+            this.btn_display_sales = new System.Windows.Forms.Button();
             this.btn_create_file = new System.Windows.Forms.Button();
             this.btn_total_sales = new System.Windows.Forms.Button();
             this.btn_average = new System.Windows.Forms.Button();
@@ -44,18 +44,18 @@
             this.textBoxInfo.Location = new System.Drawing.Point(227, 41);
             this.textBoxInfo.Multiline = true;
             this.textBoxInfo.Name = "textBoxInfo";
-            this.textBoxInfo.Size = new System.Drawing.Size(303, 410);
+            this.textBoxInfo.Size = new System.Drawing.Size(672, 410);
             this.textBoxInfo.TabIndex = 0;
             // 
-            // txtBoxBasicInfo
+            // btn_display_sales
             // 
-            this.txtBoxBasicInfo.Location = new System.Drawing.Point(34, 105);
-            this.txtBoxBasicInfo.Name = "txtBoxBasicInfo";
-            this.txtBoxBasicInfo.Size = new System.Drawing.Size(158, 47);
-            this.txtBoxBasicInfo.TabIndex = 1;
-            this.txtBoxBasicInfo.Text = "Display Sales Data File";
-            this.txtBoxBasicInfo.UseVisualStyleBackColor = true;
-            this.txtBoxBasicInfo.Click += new System.EventHandler(this.txtBoxBasicInfo_Click);
+            this.btn_display_sales.Location = new System.Drawing.Point(34, 105);
+            this.btn_display_sales.Name = "btn_display_sales";
+            this.btn_display_sales.Size = new System.Drawing.Size(158, 47);
+            this.btn_display_sales.TabIndex = 1;
+            this.btn_display_sales.Text = "Display Sales Data File";
+            this.btn_display_sales.UseVisualStyleBackColor = true;
+            this.btn_display_sales.Click += new System.EventHandler(this.txtBoxBasicInfo_Click);
             // 
             // btn_create_file
             // 
@@ -93,15 +93,17 @@
             this.btn_clear.TabIndex = 5;
             this.btn_clear.Text = "Clear";
             this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
             // 
             // btn_exit
             // 
-            this.btn_exit.Location = new System.Drawing.Point(454, 465);
+            this.btn_exit.Location = new System.Drawing.Point(824, 466);
             this.btn_exit.Name = "btn_exit";
             this.btn_exit.Size = new System.Drawing.Size(75, 23);
             this.btn_exit.TabIndex = 6;
             this.btn_exit.Text = "Exit";
             this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
             // 
             // btn_lowest
             // 
@@ -125,7 +127,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(579, 519);
+            this.ClientSize = new System.Drawing.Size(937, 519);
             this.Controls.Add(this.btn_highest);
             this.Controls.Add(this.btn_lowest);
             this.Controls.Add(this.btn_exit);
@@ -133,7 +135,7 @@
             this.Controls.Add(this.btn_average);
             this.Controls.Add(this.btn_total_sales);
             this.Controls.Add(this.btn_create_file);
-            this.Controls.Add(this.txtBoxBasicInfo);
+            this.Controls.Add(this.btn_display_sales);
             this.Controls.Add(this.textBoxInfo);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -146,7 +148,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBoxInfo;
-        private System.Windows.Forms.Button txtBoxBasicInfo;
+        private System.Windows.Forms.Button btn_display_sales;
         private System.Windows.Forms.Button btn_create_file;
         private System.Windows.Forms.Button btn_total_sales;
         private System.Windows.Forms.Button btn_average;
--- SalesData1/Form1.cs
+++ SalesData1/Form1.cs
@@ -28,33 +28,42 @@ namespace SalesData1
         {
             // bool to check if create file has been clicked
             wasFileCreateBtnPressed = true;
+            btn_create_file.Enabled = false;
             createFile(wasFileCreateBtnPressed);
         }
-
-        private void displaylabelTop()
-        {
-            textBoxInfo.Text += "-Sales Data-" + Environment.NewLine;
-        }
-
 
         // button 2
         private void txtBoxBasicInfo_Click(object sender, EventArgs e)
         {
+            btn_display_sales.Enabled = false;
             // checks if user hits button 2 first then it deletes any file there and forces user to create
             if (wasFileCreateBtnPressed == false)
             {
                 MessageBox.Show("Please click Create File button first.");
             }
-            
+
             //createFile(wasFileCreateBtnPressed);
 
             if (wasFileCreateBtnPressed == true)
             {
-                // displays header info first
-                displaylabelTop();
                 // gathers data first storing into arrays in a method
                 storeAllData();
+                writeToDisplay(workerNames, salesDataNumbers, daysOfWeek);
             }
+        }
+
+        // clear form
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            textBoxInfo.Clear();
+            btn_display_sales.Enabled = true;
+            btn_create_file.Enabled = true;
+        }
+
+        // Close Form
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
 
         private void createFile(bool isButton1clicked)
@@ -65,8 +74,8 @@ namespace SalesData1
 
             // creates a file (this is needed so the application can be self-reliant)
             string[] fileData = {"100", "70", "80", "60", "70", "90",
-                "100", "100", "90", "50", "70", "80", "90", "100", "51", "70", "75", "70", "80", "90",
-                "Jim", "Kristina", "Darryl", "William", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+                "100", "100", "95", "50", "70", "80", "90", "100", "51", "70", "75", "70", "80", "90",
+                "Jim", "Kathleen", "Darryl", "William", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
 
             string fileCheck = fileLocation;
             if (File.Exists(fileCheck))
@@ -102,9 +111,9 @@ namespace SalesData1
             // store all numeric data
             using (StreamReader read = new StreamReader(fileLocation))
             {
-                // Read and display lines from the file until the end of
+                // Read and display lines from the file until the end of...
                 // the file is reached.
-                for (int row = 0; row < 4 ; row++)
+                for (int row = 0; row < 4; row++)
                 {
                     for (int col = 0; col < 5; col++)
                     {
@@ -122,19 +131,52 @@ namespace SalesData1
                 {
                     daysOfWeek[row] = read.ReadLine();
                 }
-                read.Close(); ;
+                read.Close();
 
                 // call writeToDisplay here
             }
         }
 
-        // write data to text box
-        private void writeToDisplay(string[] names, int[] sales, int days)
+        private void displaylabelTop()
         {
+            textBoxInfo.Text += "Sales Data Analysis" + Environment.NewLine;
+            textBoxInfo.Text += Environment.NewLine;
+            textBoxInfo.Text += "Base Data" + Environment.NewLine;
+            textBoxInfo.Text += Environment.NewLine;
+        }
+
+        // write data to text box
+        private void writeToDisplay(string[] names, int[,] sales, string[] days)
+        {
+            // displays header info first
+            displaylabelTop();
+
             // print days
+            textBoxInfo.Text += "\t";
+            for (int i = 0; i < 5; i++)
+            {
+                textBoxInfo.Text += "\t";
+                textBoxInfo.Text += days[i];
+                textBoxInfo.Text += "\t";
+            }
 
-
+            textBoxInfo.Text += Environment.NewLine;
             // print name and sales
+            int nameLength = names.Length;
+          
+            for (int row = 0; row < 4; row++)
+            {
+                textBoxInfo.Text += Environment.NewLine;
+                textBoxInfo.Text += names[row];
+                textBoxInfo.Text += "\t";
+
+                for (int col = 0; col < 5; col++)
+                {
+                    textBoxInfo.Text += "\t";
+                    textBoxInfo.Text += sales[row, col].ToString("n");
+                    textBoxInfo.Text += "\t";
+                }
+            }
         }
     }
 }
